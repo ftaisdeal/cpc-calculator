2baa75e271457b966c2b88a7463a4b739cfcdf7c
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2023-09-08 14:55:58 -0400

    add meta description, replace favicon

--- components.js
+++ components.js
@@ -1,4 +1,4 @@
-const header = function (title = "Adventure Cabaret", short_description = "Adventure Cabaret", og_image = "/OG-default.png") {
+const header = function (title = "Adventure Cabaret", short_description = "Adventure Cabaret is a live multitalent, multimedia show that recruits local talent of all kinds and weaves different types of live performance together into a compelling set of deep experiences that also include audience participation in varied ways.", og_image = "/OG-default.png") {
 
   return (`<!DOCTYPE HTML>
 <html lang="en">
@@ -6,6 +6,7 @@ const header = function (title = "Adventure Cabaret", short_description = "Adven
 <meta charset="utf-8">
 <title>${title}</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" type="image/png" href="/favicon.png" sizes="140x140">
 <meta name="description" content="${short_description}">
 <meta property="og:type" content="article">
 <meta property="og:title" content="${title}">
@@ -16,7 +17,7 @@ const header = function (title = "Adventure Cabaret", short_description = "Adven
 <meta property="og:image:width" content="600">
 <meta property="og:image:height" content="600">
 <meta property="og:url" content="">
-<meta property="article:published_time" content="2023-07-01">
+<meta property="article:published_time" content="2023-09-08">
 <meta name="author" content="Miranda Sorventi">
 <link rel="stylesheet" href="/styles.css">
 <link rel="preconnect" href="https://fonts.googleapis.com">
