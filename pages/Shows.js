a595931eaff536f5b1a90aa500b92b10fe343515
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2023-09-08 20:55:18 -0400

    adjust content of "shows" page

--- pages/Shows.js
+++ pages/Shows.js
@@ -6,7 +6,7 @@ const Home = async function (res) {
 
 <p>Adventure Cabaret is not just one show but a series of shows, each one unique. The shows are all different, but they all have the same goal: to provide excellent, truly useful ideas to help make the world better for all of us.</p>
 
-<p>We currently have four entirely different shows written, and we are working on more. We are always working on new material, and we are always working on new ways to present it.</p>
+<p>We currently have four entirely different shows written, and we have even more planned. We are always working on new material, and always working on new ways to present it.</p>
 
 <p>We hope you'll join us again and again, as we continue to explore and grow together. With your help, we can all make the world smarter, more trustworthy, more creative and more kind.</p>
 
