23b73299f19658e12afbe9cf9635e25c4b6613f3
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2023-10-21 13:03:37 -0400

    edit home page

--- pages/Home.js
+++ pages/Home.js
@@ -3,13 +3,15 @@ const Home = async function (res) {
   const { header, footer } = require('../components');
 
   const content = `<h1>Home</h1>
-  <p>Adventure Cabaret is a unique and unprecedented live theater experience that includes massive use of multimedia to bring you into the Empire of Ideas, and gradually transform you into an alien who can help save humanity from itself.</p>
+  <p>Adventure Cabaret is a unique live theater experience that includes massive use of multimedia to bring you into the Empire of Ideas, and gradually transform you into an alien who can help save humanity from itself.</p>
   
-  <p>As if that weren't enough, you will also be massively entertained by an astral LED fan artist, a miraculous disabled guitar player, a heavy metal band improvising to the longest list of human vices you have ever heard, and great actors performing deep scenes of love and pain and joy and failure and transformation and redemption.</p>
+  <p>As if that weren't enough, you will also be entertained by an astral LED fan artist, a miraculous disabled guitar player, a heavy metal band improvising to the longest list of human vices ever compiled, and great actors performing deep scenes of love and pain and joy and failure and transformation and redemption.</p>
   
-  <p>Isn't it about time somebody did this?  We think so, and we're doing it!</p>
+  <p>Isn't it about time somebody did this?  We think so.</p>
   
-  <p>Take a mere 90 minutes out of your week and go home with a head full of amazing experiences and beautiful ideas.  See you at the show!</p>
+  <p>Take a mere 90 minutes out of your week and go home with a head full of amazing experiences and beautiful ideas.</p>
+  
+  <p>See you at the show!</p>
     <img src="images/bee-in-hula.jpg">`;
 
   res.send(`${header('Adventure Cabaret')}
