a544da14be908d01cd8a00050a1c9422423b034f
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2023-10-21 09:55:49 -0400

    change home page, add audition excerpts

--- pages/Home.js
+++ pages/Home.js
@@ -3,12 +3,13 @@ const Home = async function (res) {
   const { header, footer } = require('../components');
 
   const content = `<h1>Home</h1>
-  <p>Adventure Cabaret is a live multitalent, multimedia show that recruits local talent of all kinds and
-    weaves different types of live performance together into a compelling set of deep experiences that also include
-    audience participation in varied ways.</p>
-
-  <p>Adventure and a spirit of courageous exploration are at the heart of Adventure Cabaret: adventure mentally,
-    emotionally, culturally, artistically, but most of all in considering new and better ways of life for us all.</p>
+  <p>Adventure Cabaret is a unique and unprecedented live theater experience that includes massive use of multimedia to bring you into the Empire of Ideas, and gradually transform you into an alien who can help save humanity from itself.  No, we are not kidding!</p>
+  
+  <p>As if that weren't enough, you will also be massively entertained by an astral LED fan artist, a miraculous disabled guitar player, a heavy metal band improvising to the longest list of vices you have ever heard, and great actors performing deep scenes of love and pain and joy and failure and transformation and redemption.</p>
+  
+  <p>Isn't it about time somebody did this?  We think so, and we're doing it!</p>
+  
+  <p>Take a mere 90 minutes out of your week and go home with a head full of amazing experiences and beautiful ideas.  See you at the show!</p>
     <img src="images/bee-in-hula.jpg">`;
 
   res.send(`${header('Adventure Cabaret')}
