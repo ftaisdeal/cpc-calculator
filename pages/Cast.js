4b97a50c89222f10e80472a87ed3a1232722002f
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2023-11-03 13:39:22 -0400

    edit title of cast docs page

--- pages/Cast.js
+++ pages/Cast.js
@@ -3,14 +3,14 @@ const Cast = async function (res) {
   const { header, footer } = require('../components');
 
   const content = `<br>
-  <h1>Documents and Support for Cast</h1>
+  <h1>Documents and Support for the Cast</h1>
   <ol>
   <li><a href="schedule.pdf" target="_blank">Rehearsal and Performance Schedule</a></li>
   <li><a href="role_assignments.pdf" target="_blank">Role Assignments</a></li>
   <li>Complete Script - available December 1</li>
   </ol>`;
 
-  res.send(`${header('Documents and Support for Cast')}
+  res.send(`${header('Documents and Support for the Cast')}
 ${content}
 ${footer}`);
 
