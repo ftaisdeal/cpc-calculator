88e00c79840b8b5b40f3d94475f8d9427d5aaa00
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2023-11-03 11:23:27 -0400

    udate href in cast docs

--- pages/Cast.js
+++ pages/Cast.js
@@ -6,7 +6,7 @@ const Cast = async function (res) {
   <h1>Documents and Support for Cast</h1>
   <ol>
   <li><a href="schedule.pdf" target="_blank">Rehearsal and Performance Schedule</a></li>
-  <li><a href="role_assignments.pdf target="_blank">Role Assignments</a></li>
+  <li><a href="role_assignments.pdf" target="_blank">Role Assignments</a></li>
   </ol>`;
 
   res.send(`${header('Documents and Support for Cast')}
