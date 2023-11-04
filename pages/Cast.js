f3ff23db676172b8694f62cce3b45594526c12ee
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2023-11-04 19:28:32 -0400

    add link to PDF of template for cast

--- pages/Cast.js
+++ pages/Cast.js
@@ -6,6 +6,7 @@ const Cast = async function (res) {
   <h1>Documents and Support for the Cast</h1>
   <ol>
   <li><a href="schedule.pdf" target="_blank">Rehearsal and Performance Schedule</a></li>
+  <li><a href="cast.pdf" target="_blank">Cast</a></li>
   <li><a href="role_assignments.pdf" target="_blank">Role Assignments</a></li>
   <li>Complete Script - available December 1</li>
   </ol>`;
