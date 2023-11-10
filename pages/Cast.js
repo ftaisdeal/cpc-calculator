6b7681f5e90e590af500b75e9d95446960b4004f
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2023-11-10 09:41:08 -0500

    Add links to Shotgun Studios and Google Maps

--- pages/Cast.js
+++ pages/Cast.js
@@ -11,7 +11,7 @@ const Cast = async function (res) {
   <li><a href="rundown.pdf" target="_blank">Rundown</a></li>
   <li>Rehearsals
   <br>
-  All rehearsals are held at Shotgun Studios, 1201 University Ave., Berkeley
+  All rehearsals are held at <a href="https://shotgunplayers.org/Online/default.asp?BOparam::WScontent::loadArticle::permalink=studios&BOparam::WScontent::loadArticle::context_id=" target="_blank">Shotgun Studios</a>, <a href="https://www.google.com/maps/place/1201+University+Ave,+Berkeley,+CA+94702/@37.8697402,-122.2919355,17z/data=!3m1!4b1!4m6!3m5!1s0x80857ec01833c105:0xb7f10cd0c054a07b!8m2!3d37.8697402!4d-122.2893606!16s%2Fg%2F11b8v698yp?entry=ttu" target="_blank">1201 University Ave., Berkeley</a>
     <ul>
     <li><a href="rehearsal-1.pdf" target="_blank">Rehearsal 1 - Saturday, January 6, 1-5PM</a></li>
     <li>Rehearsal 2 - Saturday, January 13, 1-5PM</li>
