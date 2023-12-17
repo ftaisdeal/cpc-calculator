53e085c9ffc02b6c7a4a83d556b0a4ec25af74b6
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2023-12-17 18:52:17 -0500

    Add "tickets" page in navigation

--- pages/Tickets.js
+++ pages/Tickets.js
@@ -9,13 +9,13 @@ Freight &amp; Salvage<br>
 2020 Addison St.<br>
 Berkeley, CA
 <p>
-<b><a href="" target="__blank">Saturday, February 10, 2024</a></b><br>
+<b><a href="https://www.eventbrite.com/e/adventure-cabaret-tickets-778918776027" target="__blank">Saturday, February 10, 2024</a></b><br>
 7:00PM<br>
 International House - Chevron Auditorium<br>
 2299 Piedmont Ave.<br>
 Berkeley, CA
 <p>
-<b><a href="" target="__blank">Saturday, February 24, 2024</a></b><br>
+<b><a href="https://www.eventbrite.com/e/adventure-cabaret-tickets-778919668697" target="__blank">Saturday, February 24, 2024</a></b><br>
 7:00PM<br>
 International House - Chevron Auditorium<br>
 2299 Piedmont Ave.<br>
