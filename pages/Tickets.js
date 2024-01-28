a0328f3d67974ce2148a3af7222620865ddb0d20
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2024-01-28 11:45:50 -0800

    Switch ticketing to Luma, modify main nav

--- pages/Tickets.js
+++ pages/Tickets.js
@@ -4,41 +4,24 @@ const Tickets = async function (res) {
 
   const content = `<h1>Tickets</h1>
 
-<b><a href="https://www.eventbrite.com/e/adventure-cabaret-tickets-778918776027" target="__blank">Saturday, February 10, 2024</a></b><br>
+<b><a href="https://lu.ma/ab87i67h" target="__blank">Saturday, February 10, 2024</a></b><br>
 7:00PM<br>
 International House - Chevron Auditorium<br>
 2299 Piedmont Ave.<br>
 Berkeley, CA
 <p>
-<div id="eventbrite-widget-container-778918776027"></div>
-
-<script src="https://www.eventbrite.com/static/widgets/eb_widgets.js"></script>
-
-<script type="text/javascript">
-    var exampleCallback = function() {
-        console.log('Order complete!');
-    };
-
-    window.EBWidgets.createWidget({
-        // Required
-        widgetType: 'checkout',
-        eventId: '778918776027',
-        iframeContainerId: 'eventbrite-widget-container-778918776027',
-
-        // Optional
-        iframeContainerHeight: 425,  // Widget height in pixels. Defaults to a minimum of 425px if not provided
-        onOrderComplete: exampleCallback  // Method called when an order has successfully completed
-    });
-</script>
-
-<!--
-<b><a href="https://www.eventbrite.com/e/adventure-cabaret-tickets-778919668697" target="__blank">Saturday, February 24, 2024</a></b><br>
-7:00PM<br>
-International House - Chevron Auditorium<br>
-2299 Piedmont Ave.<br>
-Berkeley, CA-->
-
-`;
+Please enter your coupon code in UPPER CASE.
+<p>
+<iframe
+  src="https://lu.ma/embed-checkout/evt-fPZaxiF5o8H1H95"
+  width="500"
+  height="860"
+  frameborder="0"
+  style="border: 1px solid #bfcbda88; border-radius: 4px;"
+  allowfullscreen=""
+  aria-hidden="false"
+  tabindex="0"
+></iframe>`;
 
   res.send(`${header('Tickets for Adventure Cabaret')}
 ${content}
