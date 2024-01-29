d27e0a4b59f6133a5eb8f713fe5b2f94117ae16a
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2024-01-29 11:42:47 -0800

    Revise home page

--- pages/Home.js
+++ pages/Home.js
@@ -3,18 +3,16 @@ const Home = async function (res) {
   const { header, footer } = require('../components');
 
   const content = `<h1>What the heck is this?</h1>
+Adventure Cabaret is a unique theater experience, combining live actors with immersive multimedia.
+
 <p>Adventure Cabaret is a journey through the Empire of the Mind.</p>
 
 <p>Adventure Cabaret is an escape from the prison of nostalgia.</p>
   
 <p>Adventure Cabaret is an exploration of how much better humanity could become, if only it chose to do so.</p>
   
-<p>Adventure Cabaret is a grand tour through all of space, all of time, and all of your personality.</p>
+<p>Spend 90 minutes with us, and go home with lots of laughs and a head full of beautiful and inspiring new ideas.</p>
 
-<p>Adventure Cabaret is loads of fun, and at least two barrels full of laughs.</p>
-  
-<p>Take a mere 90 minutes out of your week and go home with a head full of amazing experiences and beautiful ideas.</p>
-  
 <p>See you at the show!</p>
 
 <img src="images/bee-in-hula.jpg" height="497" width="538" alt="bee flying through LED hula hoop">`;
