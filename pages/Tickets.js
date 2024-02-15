cfc89493e3088771dd123e9e2813470d43a20504
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2024-02-15 09:54:38 -0800

    Add .button to CSS, edit Tickets page

--- pages/Tickets.js
+++ pages/Tickets.js
@@ -3,9 +3,32 @@ const Tickets = async function (res) {
   const { header, footer } = require('../components');
 
   const content = `<h1>Tickets</h1>
-Currently, no performances are scheduled.
-<p>
-We plan to resume production in a few months.`;
+
+  <p>No full performances of Adventure Cabaret are currently scheduled, but we develop our material partly through storytelling events with our sister site, <a href="https://storytelling.social">Storytelling Social</a>.</p>
+
+  <p>The next Storytelling Social event is on Tuesday, March 5, 7:30-9:00pm.</p>
+
+  <p>The venue is a small dance studio at Market and Jones.</p>
+
+  <noscript><a href="https://www.eventbrite.com/e/storytelling-social-starter-event-tickets-839546214327"
+          rel="noopener noreferrer" target="_blank">Buy Tickets on Eventbrite</a></noscript>
+      <button id="eventbrite-widget-modal-trigger-839546214327" class="button" type="button">Get Tickets</button>
+
+      <script src="https://www.eventbrite.com/static/widgets/eb_widgets.js"></script>
+
+      <script type="text/javascript">
+        var exampleCallback = function () {
+          console.log('Order complete!');
+        };
+
+        window.EBWidgets.createWidget({
+          widgetType: 'checkout',
+          eventId: '839546214327',
+          modal: true,
+          modalTriggerElementId: 'eventbrite-widget-modal-trigger-839546214327',
+          onOrderComplete: exampleCallback
+        });
+      </script>`;
 
   res.send(`${header('Tickets for Adventure Cabaret')}
 ${content}
