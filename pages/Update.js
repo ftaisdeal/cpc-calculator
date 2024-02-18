b6701b522d47ec48e5e8b03e402830c91541642c
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2024-02-18 09:18:08 -0800

    Update the "update" page

--- pages/Update.js
+++ pages/Update.js
@@ -3,20 +3,27 @@ const Update = async function (res) {
   const { header, footer } = require('../components');
 
   const content = `<h3>Update on the Production</h3>
+<i>Updated: Sunday, February 18, 2024</i>
+
+<p>
+
 <ol>
-<li>A new media form called a "QuoteMovie" will be included in the production at various points. A QuoteMovie is a movie made only of quotes.  Despite consisting only of white words on a black screen, these movies have powerful psychological and emotional effects.  Here is <a href="https://youtu.be/JOdb-rvFzqI" target="_blank">a quick example</a>.</li>
+<li>The production is being recast from scratch, which means that it will be a few months before we can open the show.</li>
+<li>The show will open in previews of segments from the production, a roughly 50/50 mix of live acting and multimedia.</li>
+<li>These previews will be used to further develop and refine the production, and you can be part of this process.</li>
+<li>The venues will all be in San Francisco.</li>
+<li>These limited previews will be only 30-45 minutes long, only involve three actors, and will take place in small, intimate venues such as dance and yoga studios.</li>
+<li>Maximum seating will be only about 20.</li>
 <li>We are now developing new material through our sister site, <a href="https://bit.ly/42PGVWj" target="__blank">Storytelling Social</a>.</li>
 <li>The first storytelling event through Storytelling Social is now scheduled for Tuesday, March 5, 7:30-9:00pm.</li>
 <li>We will also use the <a href="https://bit.ly/42PGVWj" target="__blank">Storytelling Social</a> site for cross-promotion.
 <li>We've added an <a href="https://www.instagram.com/adventurecabaret" target="__blank">Instagram account</a>.
-<li>The production is being recast from scratch.</li>
-<li>The show will open in previews of segments from the show, a roughly 50/50 mix of live acting and multimedia.</li>
-<li>The venues will all be in San Francisco.</li>
-<li>These limited previews will be only 30-45 minutes long, only involve three actors, and will take place in small, intimate venues such as dance and yoga studios.</li>
-<li>Maximum seating will be only about 20.</li>
 <li>We have purchased a video projector so that we can perform the show in almost any space.</li>
 <li>We will have complete control of theatrical lighting, including dimmability, color control and special effects such as strobe and "police lights" in nearly any space we choose through a lighting kit that is controllable from a laptotp or phone over WiFi or Bluetooth.</li>
+<li>A new media form called a "QuoteMovie" will be included in the production at various points. A QuoteMovie is a movie made only of quotes.  Despite consisting only of white words on a black screen, these movies have powerful psychological and emotional effects.  Here is <a href="https://youtu.be/JOdb-rvFzqI" target="_blank">a quick example</a>.</li>
 </ol>
+
+</p>
 
 <h3>Venues</h3>
 <ol>
