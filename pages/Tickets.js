f448684591910deccfaebeb47ce30a2802026192
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2024-07-10 18:08:41 -0400

    Edit tickets page

--- pages/Tickets.js
+++ pages/Tickets.js
@@ -4,10 +4,7 @@ const Tickets = async function (res) {
 
   const content = `<h1>Tickets</h1>
 
-<p>No full performances of Adventure Cabaret are currently scheduled, but we will be resuming production soon.</p>
-      
-<br>
-<br>`;
+<p>No full performances of Adventure Cabaret are currently scheduled, but we will be resuming production soon.</p>`;
 
   res.send(`${header('Tickets for Adventure Cabaret')}
 ${content}
