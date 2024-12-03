21a7999ea391e02a6c99bdd6ba635e3eafd1ce4b
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2024-12-02 16:08:21 -0800

    Update "Update" page and tickets page

--- pages/Tickets.js
+++ pages/Tickets.js
@@ -8,7 +8,7 @@ const Tickets = async function (res) {
 
 <p>Please check back about tickets in early January for an opening probably in the first weeks of March.</p>
 
-<p>In the mean time, we are holding a small number of public rehearsals at Phoenix Theater in San Francisco's theater district, the first of which is on Sunday, December 8, 10am-2pm. Free tickets are available <a href="https://www.eventbrite.com/e/adventure-cabaret-public-rehearsal-tickets-1097685259159">here</a>.</p>`;
+<p>In the mean time, we are holding a small number of public rehearsals at Phoenix Theater in San Francisco's theater district, the first of which is on Sunday, December 8, 10am-2pm. <a href="https://www.eventbrite.com/e/adventure-cabaret-public-rehearsal-tickets-1097685259159">Free tickets are available here</a>.</p>`;
 
   res.send(`${header('Tickets for Adventure Cabaret')}
 ${content}
--- pages/Update.js
+++ pages/Update.js
@@ -12,7 +12,7 @@ const Update = async function (res) {
 <li>The new actors are absolutely great and we are very thrilled to be working with them.</li>
 <li>We are in preliminary rehearsals at Phoenix Theater in San Francisco's theater district.</li>
 <li>We are planning to open at Theater 33, 533 Sutter St., a great 100-seat theater, in early March.</li>
-<li>We will be holding a number of public rehearsals at Phoenix Theater.</li>
+<li>We will be holding a number of public rehearsals at Phoenix Theater, the first of which is on Sunday, December 8, 10am-2pm. <a href="https://www.eventbrite.com/e/adventure-cabaret-public-rehearsal-tickets-1097685259159">Free tickets are available here</a>.</li>
 </ol>`;
 
   res.send(`${header('Update on the Production')}
