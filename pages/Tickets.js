6011922e2c3775a9b6ccdc26407ae1374ba56f41
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2024-12-02 16:05:23 -0800

    Update "Update" page

--- pages/Tickets.js
+++ pages/Tickets.js
@@ -4,9 +4,9 @@ const Tickets = async function (res) {
 
   const content = `<h1>Tickets</h1>
 
-<p>Adventure Cabaret goes into full production in mid-January 2025.</p>
+<p>Adventure Cabaret goes into full production rehearsals in January 2025.</p>
 
-<p>Please check back about tickets in early January.  Thank you.</p>
+<p>Please check back about tickets in early January for an opening probably in the first weeks of March.</p>
 
 <p>In the mean time, we are holding a small number of public rehearsals at Phoenix Theater in San Francisco's theater district, the first of which is on Sunday, December 8, 10am-2pm. Free tickets are available <a href="https://www.eventbrite.com/e/adventure-cabaret-public-rehearsal-tickets-1097685259159">here</a>.</p>`;
 
