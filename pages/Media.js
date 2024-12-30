8cf8cb632a51f0e1485173ed4fa1a8f62394af1e
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2024-12-30 09:38:49 -0800

    Add item list to media page

--- pages/Media.js
+++ pages/Media.js
@@ -4,6 +4,11 @@ const Origins = async function (res) {
 
   const content = `<h1>Media Kit</h1>
   <ol>
+  <li>Press release</li>
+  <li>Marketing graphics</li>
+  <li>Rehearsal pictures and video</li>
+  <li>About</li>
+  <li>Miranda Sorventi bio</li>
  </ol>`;
 
   res.send(`${header('Media Kit for Adventure Cabaret')}
