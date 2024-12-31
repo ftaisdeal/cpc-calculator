c647e88d4255d7a6263cf4492dd8712191faa75e
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2024-12-30 18:27:42 -0800

    Remove SendMail from Error.js

--- pages/Error.js
+++ pages/Error.js
@@ -4,8 +4,6 @@ const { header } = require('../components');
 
 const Error = function (res, error, message) {
 
-  //sendEmail('firinn@taisdeal.com', 'Sustainable Choices: Error Alert', error, '');
-
   const content = `<h1>Error</h1>
 <p>${message}</p>`;
 
