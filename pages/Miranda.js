4fa1186372558c67019f1c7fc0cb344a21eaca0d
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2025-01-06 16:34:02 -0800

    Add to Miranda page

--- pages/Miranda.js
+++ pages/Miranda.js
@@ -5,7 +5,12 @@ const Home = async function (res) {
   const content = `<h1>Miranda Sorventi</h1>
   Miranda Sorventi was born in 1985 in a small village in Italy, coincidentally only a few kilometers from where Leonardo da Vinci was born. From an early age it was clear that Miranda possessed exceptional intelligence, as well as deep artistic gifts. Miranda taught herself to read before she could speak, and was late to begin speaking at all. When she did begin speaking at the age of four she spoke in complete, lengthy sentences of grammatical perfection, tinged with humor and deliberate ambiguity. It was also at the age of four that she completed her first drawing, an astonishing, highly detailed sepia of a fruit fly, which she labeled "<i>drosophila</i> deliberately in the style of Leonardo."
   <p>
-  Miranda's parents, Arturo and Gabriella, were both artisans&mdash;he in pottery, and she in jewelery. Both were relatively uneducated, having barely graduated from high school, but Miranda grew up in an environment of creativity and fine esthetics. There was a small library in the village, and beginning at the age of five Miranda would visit the library daily, reading everything she could. There was a large dictionary of Italian in the library, which Miranda could frequently be seen studying with great intensity.`;
+  Miranda's parents, Arturo and Gabriella, were both artisans&mdash;he in pottery, and she in jewelery. Both were relatively uneducated, having barely graduated from high school, but Miranda grew up in an environment of creativity and fine esthetics.
+  </p>
+  <p>
+  There was a small library in the village, and beginning at the age of five Miranda would visit the library daily, reading everything she could. There was a large dictionary of Italian in the library, which Miranda could frequently be seen studying with great intensity. Miranda's gifts of both memory and creative language soon began to emerge. The librarian playfully challenged Miranda to define words from random pages of the dictionary, and was shocked to find that Miranda could remember every definition she had read. Soon Miranda began making jokes based on her own creative variations of advanced vocabulary. She would laugh heartily at her own jokes, but her parents did not understand her jokes, which really could only be understood by Miranda and experts is semantics and etymology. This all began as Miranda turned six year old.
+  </p>
+  `;
 
   res.send(`${header('Adventure Cabaret')}
 ${content}
