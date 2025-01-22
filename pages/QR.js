7db2270561a00c798b6dcef8d69221124cedc3e4
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2025-01-22 15:39:20 -0800

    Add code for reading query string for QR codes

--- pages/QR.js
+++ pages/QR.js
@@ -1,13 +1,31 @@
-const Origins = async function (req, res) {
-
+const QR = async function (req, res) {
   const { header, footer } = require('../components');
+  const page = parseInt(req.query.page, 10);
 
-  const content = `<h1>QR Codes</h1>`;
+  switch (page) {
+    case 1:
+      title = "QR Code 1";
+      text = "Text for QR Code 1";
+      break;
+    case 2:
+      title = "QR Code 2";
+      text = "Text for QR Code 2";
+      break;
+    case 3:
+      title = "QR Code 3";
+      text = "Text for QR Code 3";
+      break;
+    default:
+      title = "No QR Code Specified";
+  } 
 
-  res.send(`${header('QR Codes')}
+  const content = `<h1>${title}</h1>
+  ${text}`;
+
+  res.send(`${header(`${title}`)}
 ${content}
 ${footer}`);
 
 }
 
-module.exports = Origins;
+module.exports = QR;
