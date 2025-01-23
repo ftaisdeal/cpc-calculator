2d97fdb29545f4d85342e08cd309c95d922b484d
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2025-01-23 09:15:50 -0800

    Revise QR pages 1 and 2

--- pages/QR.js
+++ pages/QR.js
@@ -5,11 +5,11 @@ const QR = async function (req, res) {
   switch (page) {
     case 1:
       title = "One Teaspoon of Healthy Soil";
-      text = `<p>A teaspoon of healthy soil can contain thousands of different species of microorganisms, and a total of A single teaspoon approximately 1 billion to 10 billion organisms. These include bacteria, fungi, protozoa, nematodes, and microarthropods. During human childhood, ontact with organisms typically found in soil is crucial for the development of a healthy immune system.</p>`;
+      text = `<p>A single teaspoon of healthy soil can contain thousands of different species of microorganisms, and a total of A single teaspoon approximately 1 billion to 10 billion organisms. These include bacteria, fungi, protozoa, nematodes, and microarthropods. During human childhood, ontact with organisms typically found in soil is crucial for the development of a healthy immune system.</p>`;
       break;
     case 2:
       title = "Bryophytes Are Important";
-      text = `<p>Bryophytes are a group of non-vascular plants that include mosses, liverworts, and hornworts. Brophytes do not have a vascular system, and this lack of a vascular system strictly limits the size to which they can grow.`;
+      text = `<p>Bryophytes are a ubiquitous group of non-vascular plants that include mosses, liverworts, and hornworts. Brophytes do not have a vascular system, and this lack of a vascular system strictly limits the size to which they can grow.`;
       break;
     case 3:
       title = "Please Turn Off the Audio on Your Phone";
