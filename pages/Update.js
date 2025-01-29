88c1f99d8fbcc99d38a824365079e11011b2f4eb
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2025-01-29 06:51:17 -0800

    Update the "Update" page

--- pages/Update.js
+++ pages/Update.js
@@ -11,8 +11,9 @@ const Update = async function (res) {
 <li>We will be having a <a href="https://www.eventbrite.com/e/adventure-cabaret-picnic-rehearsal-in-dolores-park-tickets-1225652281969" target="__blank">public picnic in Dolores Park on March 2</a>, the day after the first performance, from 12-4pm. Feel free to join us.</li>
 <li>The show has been strongly revised based on our experience with the pilot production last year.</li>
 <li>Many new elements have been added to the show.</li>
-<li>We are done with casting the full production version of the show, and will soon also have backup casting in place.</li>
-<li>The new actors are absolutely great and we are very thrilled to be working with them.</li>
+<li>The multimedia assets are almost done, and they are amazing.</li>
+<li>We have added some choreography with flashlights to the open of the show.</li>
+<li>We are beginning special training for the actors in order to become a "power team" of actors.</li>
 </ol>
 <br>
 <img src="images/hand-print.png" width="400" height="488" alt="purple hand print">`;
