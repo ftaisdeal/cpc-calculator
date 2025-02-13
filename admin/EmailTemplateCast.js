2994a3a3eec922bf4b72f833d6e7413d0947fd84
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2025-02-13 08:07:05 -0800

    Add EmailUpdate.js to .gitignore

---
+++ admin/EmailTemplateCast.js
@@ -0,0 +1,53 @@
+const EmailTemplateCast = (first_name) => {
+
+body = ``;
+
+  const message = `<html>
+<head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Nothing+You+Could+Do&display=swap" rel="stylesheet">
+<style>
+body {
+font-family: Verdana, Arial, sans-serif;
+font-size: 1em;
+max-width: 800px;
+}
+.header {
+  background-color: black;
+  padding: 10px;
+}
+.title {
+  font-size: 3.5em;
+  font-family: 'Nothing You Could Do', cursive;
+  line-height: normal;
+}
+.content {
+  padding: 20px;
+}
+a {
+  text-decoration: none;
+}
+.footer {
+  background-color: #8a8;
+  padding: 6px;
+}
+</style>
+</head>
+<body>
+<div class="header">
+<span class="title"> <a href="https://adventurecabaret.com"><span style="color:#674ea7;">A</span><span style="color:#6aa84f;">d<span><span style="color:#e06666;">v<span><span style="color:#ffd966;">e<span><span style="color:#3c78d8;">n<span><span style="color:#38761;">t<span><span style="color:#a64d78;">u<span><span style="color:#dd7e6b;">r<span><span style="color:#a4c2f4;">e<span> <span style="color:#cd4025;">C</span><span style="color:#fff;">abaret</span></a></span>
+</div>
+<div class="content">
+${first_name}:
+${body}
+</div>
+<div class="footer"></div>
+</body>
+</html>`;
+
+  return (`${message}`);
+
+}
+
+module.exports = EmailTemplateCast;
