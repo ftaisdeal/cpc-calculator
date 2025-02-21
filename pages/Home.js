5e3e1fab0ce94c692f51f3d7fd4259cccb8bd962
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2025-02-21 07:51:06 -0800

    Change style of ticketing button

--- pages/Home.js
+++ pages/Home.js
@@ -43,7 +43,7 @@ Phoenix Theater, 414 Mason St, San Francisco, CA 94102
 <!-- Noscript content for added SEO -->
 <noscript><a href="https://www.eventbrite.com/e/adventure-cabaret-metanoia-version-10-tickets-1255264232109" rel="noopener noreferrer" target="_blank">Buy Tickets on Eventbrite</a></noscript>
 <!-- You can customize this button any way you like -->
-<button id="eventbrite-widget-modal-trigger-1255264232109" type="button">Buy Tickets</button>
+<button id="eventbrite-widget-modal-trigger-1255264232109" type="button" style="padding:10px; border-radius: 4px; background-color: #4c4;">Buy Tickets</button>
 
 <script src="https://www.eventbrite.com/static/widgets/eb_widgets.js"></script>
 
