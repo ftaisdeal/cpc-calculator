adc2ea831045fde413f0cdde22ec98052f1b7a2c
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2025-02-21 17:12:14 -0800

    Add "required" to email submit on home page

--- pages/Home.js
+++ pages/Home.js
@@ -64,7 +64,7 @@ Phoenix Theater, 414 Mason St, San Francisco, CA 94102
 <b>Stay in touch about our progress and performances.</b>
 <form action="email" method="post">
 <input type="email" name="email" class="email" placeholder="enter email to stay in touch">
-<input type="email" name="liame" style="width: 28ch; padding: 6px; border-radius:4px;" placeholder="enter email to stay in touch"> <button type="submit" class="button">send</button>
+<input type="email" name="liame" style="width: 28ch; padding: 6px; border-radius:4px;" placeholder="enter email to stay in touch" required> <button type="submit" class="button">send</button>
 </form>
 </p>
 
