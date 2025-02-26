237e50a28eee6f59ec21fcd5f218c3e871079d5f
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2025-02-25 21:34:41 -0800

    Update email files

--- AddEmails.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO email_update (first_name, email, token) VALUES ('Lauren', 'laurensmileywriter@protonmail.com', 'f47a8854e17984dacb2943748755162c');
-INSERT INTO email_update (first_name, email, token) VALUES ('Andrew', 'info@andrewtourssf.com', '360df837fb10e0f7fc22dbccc436ffd1');
-INSERT INTO email_update (first_name, email, token) VALUES ('Roko', 'onewithitall@gmail.com', '1a3b2682b7385bd0eb7ec368aa99acbd');
-INSERT INTO email_update (first_name, email, token) VALUES ('Nomin', 'nomin.byambadorj07@gmail.com', '8f6290263e8a287e28e4fdb8965e301b');
-INSERT INTO email_update (first_name, email, token) VALUES ('Tetiana', 'gutsalyuktanya@gmail.com', '7035473aa191f7a17710f101c44d6099');
