8a177ce57a419db81f50ca755e8c270d325c6867
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2025-03-04 15:43:25 -0800

    Update "Update" page

--- pages/Update.js
+++ pages/Update.js
@@ -6,12 +6,12 @@ const Update = async function (res) {
 <i>Update: Sunday, January 19, 2025</i>
 <p>
 <ol>
-<li>The March 1st performance is sold out, but <a href="/tickets">tickets are available for a performance on March 22nd</a>.</li>
-<li>We are opening at <a href="https://www.phoenixtheatresf.org/" target="__blank">Phoenix Theater</a> in San Francisco's theater district.</li>
-<li>We will be having a <a href="https://www.eventbrite.com/e/adventure-cabaret-picnic-rehearsal-in-dolores-park-tickets-1225652281969" target="__blank">public picnic in Dolores Park on March 2</a>, the day after the first performance, from 12-4pm. Feel free to join us.</li>
-<li>The show has been strongly revised based on our experience with the pilot production last year.</li>
-<li>Many new elements have been added to the show.</li>
-<li>The multimedia assets are almost done, and they are amazing.</li>
+<li>We had a tremendously successful opening night on March 1, with a sold out show, a great performance and an appreciative and enthusiastic audience.</li>
+<li><a href="/tickets">Tickets are available for a performance on March 22nd</a>.</li>
+<li>The second performance is at <a href="https://www.phoenixtheatresf.org/" target="__blank">Phoenix Theater</a> in San Francisco's theater district.</li>
+<li>We will be moving over to <a href="https://www.theater33.com/" target="__blank">Theater 33</a>, at 533 Sutter Street, for a performance on April 5th.</li>
+<li>We are now working with a lighting designer, Beth Cockrell, who will be designing the lighting for future performances.</li>
+<li>Many enhancements are being added to the multimedia assets for the show.</li>
 <li>We have added some choreography with flashlights to the open of the show.</li>
 <li>We have begun special training for the actors in order to become a "power team" of actors.</li>
 </ol>
