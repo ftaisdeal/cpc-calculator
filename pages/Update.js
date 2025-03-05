7365fe2006f7b31af72690a3d29902cbbd02e5f4
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2025-03-04 20:05:47 -0800

    Update "Update" page for wording

--- pages/Update.js
+++ pages/Update.js
@@ -13,7 +13,7 @@ const Update = async function (res) {
 <li>We are now working with a lighting designer, Beth Cockrell, who will be designing the lighting for future performances.</li>
 <li>Many enhancements are being added to the multimedia assets for the show.</li>
 <li>We have added some choreography with flashlights to the open of the show.</li>
-<li>We have begun special training for the actors in order to become a "power team" of actors.</li>
+<li>We have begun special training for the cast in order to become a "power team" of actors.</li>
 </ol>
 <br>
 <img src="images/hand-print.png" width="400" height="488" alt="purple hand print">`;
