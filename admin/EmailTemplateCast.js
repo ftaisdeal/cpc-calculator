e46f84b813226084acf6cfadd4cfc585b994301c
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2025-03-06 12:58:14 -0800

    Add tp tje message to the cast

--- admin/EmailTemplateCast.js
+++ admin/EmailTemplateCast.js
@@ -1,38 +1,20 @@
 const EmailTemplateCast = (first_name) => {
 
-  const subject = `Adventure Cabaret: Message to the Cast - February 24, 2025`;
+  const subject = `Adventure Cabaret: Message to the Cast - March 6, 2025`;
 
-  const text = `1. I know that rehearsal yesterday was full of moments of tension, but that is normal as we approach the first performance. We now have only five days to prepare for doing a great show on Saturday. Let's all focus intensely, work smart and get the job done this week.
+  const text = `Part One: https://youtu.be/6XDAF9Hgcv8
+Part Two: https://youtu.be/cdVpAiLQQ4I
 
-2. Your personalized t-shirts arrive on Wednesday, so I will be handing them out at rehearsal on Friday. The foamcore 11x17 posters also arrive this week, and we will go over the "audience induction" plan for the show during rehearsal on Friday as well.  It's pretty simple. Most of all we have to have the spirit of fun and adventure in our hearts as we greet the audience for the first time.
+You’re fired. Wait! You’re rehired. Email us a list of things you’ve done today. Wait! Forget it. You’re fired again. Come back! Your job was important. You’re fired. Or hired. Come in to the office. Wait! The office has no computers. Go home.
 
-3. I will be cleaning the entire theater on Friday before rehearsal, part of which means thoroughly vacuuming the backstage area, and cleaning the shelves below the mirrors with a sponge.
+We are the Department of Government Efficiency.`;
 
-4. The new multimedia file for Friday rehearsal will include many more audio cues for the segments, which you will hear over the audio system. This will help to make the entrances more precise.
+const body = `Part One: https://youtu.be/6XDAF9Hgcv8
+Part Two: https://youtu.be/cdVpAiLQQ4I
 
-5. This is for the future, but Paula brought up the wonderful idea of taking a four-day weekend trip to LA and doing the show on a Friday and Saturday night in LA. We would rent a big van and all drive down together in the van, and either rent a big house or all stay in the same hotel together. I think this is a great idea, and we might be able to do this as soon as June. Let's plan for it!
+You’re fired. Wait! You’re rehired. Email us a list of things you’ve done today. Wait! Forget it. You’re fired again. Come back! Your job was important. You’re fired. Or hired. Come in to the office. Wait! The office has no computers. Go home.
 
-6. See you tonight at 7:30 for rehearsal by video: https://meet.google.com/cxn-xaxm-mng
-
-Thanks!`;
-
-const body = `<p>1. I know that rehearsal yesterday was full of moments of tension, but that is normal as we approach the first performance. We now have only five days to prepare for doing a great show on Saturday. Let's all focus intensely, work smart and get the job done this week.</p>
-
-<p>2. Your personalized t-shirts arrive on Wednesday, so I will be handing them out at rehearsal on Friday. The foamcore 11x17 posters also arrive this week, and we will go over the "audience induction" plan for the show during rehearsal on Friday as well.  It's pretty simple. Most of all we have to have the spirit of fun and adventure in our hearts as we greet the audience for the first time.</p>
-
-<p>3. I will be cleaning the entire theater on Friday before rehearsal, part of which means thoroughly vacuuming the backstage area, and cleaning the shelves below the mirrors with a sponge.</p>
-
-<p>4. The new multimedia file for Friday rehearsal will include many more audio cues for the segments, which you will hear over the audio system. This will help to make the entrances more precise.</p>
-
-<p>5. This is for the future, but Paula brought up the wonderful idea of taking a four-day weekend trip to LA and doing the show on a Friday and Saturday night in LA. We would rent a big van and all drive down together in the van, and either rent a big house or all stay in the same hotel together. I think this is a great idea, and we might be able to do this as soon as June. Let's plan for it!</p>
-
-<p>6. See you tonight at 7:30 for rehearsal by video:</p>
-
-<p><a href="https://meet.google.com/cxn-xaxm-mng">https://meet.google.com/cxn-xaxm-mng</a></p>
-
-<p>Keep flexing!</p>
-
-<br>
+We are the Department of Government Efficiency.
 
 <img src="https://adventurecabaret.com/images/brain-flex.png" width="400" height="181">`;
 
