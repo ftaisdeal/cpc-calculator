2470204ecdb237821b32585b31bce994951f3c1f
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2025-03-08 20:16:30 -0800

    Refacto EmailTracking

--- admin/email/EmailTracking.js
+++ admin/email/EmailTracking.js
@@ -1,5 +1,4 @@
 const EmailTracking = async (req, res) => {
-
   const token = req.query.token;
 
   if (token) {
@@ -9,7 +8,8 @@ const EmailTracking = async (req, res) => {
 
     try {
       // Read the current open_count value
-      const [rows] = await connection.query(`SELECT open_count FROM email_update WHERE token="${token}";`);
+      const [rows] = await connection.query('SELECT open_count FROM email_update WHERE token = ?', [token]);
+
       if (rows.length === 0) {
         res.status(404).send('Token not found.');
         return;
@@ -17,14 +17,27 @@ const EmailTracking = async (req, res) => {
 
       const open_count = rows[0].open_count;
 
+      if (open_count === undefined || open_count === null) {
+        res.status(500).send('Open count is invalid.');
+        return;
+      }
+
       // Update the open_count value and last_opened timestamp
-      const update_query = `UPDATE email_update SET open_count = ${open_count + 1}, last_opened = CURRENT_TIMESTAMP WHERE token="${token}";`;
-      await connection.query(update_query);
+      const update_query = `UPDATE email_update SET open_count = ?, last_opened = CURRENT_TIMESTAMP WHERE token = ?`;
+      const [updateResult] = await connection.query(update_query, [open_count + 1, token]);
+
+      if (updateResult.affectedRows === 0) {
+        res.status(500).send('Failed to update the open count.');
+        return;
+      }
 
     } catch (error) {
       console.error('Database error:', error);
       res.status(500).send('An error occurred while processing your request.');
       return;
+    } finally {
+      // Close the connection pool after the operation
+      await connection.end();
     }
 
     const img = Buffer.from( // Return a 1x1 transparent pixel
@@ -40,7 +53,6 @@ const EmailTracking = async (req, res) => {
   } else {
     res.status(400).send('Token parameter is required.');
   }
-
 }
 
 module.exports = EmailTracking;
