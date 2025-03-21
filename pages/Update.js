48e4d3d3d57f105af9661f909b7ac7fc57c7c6d3
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2025-03-21 11:12:56 -0700

    Update the "Update" page

--- pages/Update.js
+++ pages/Update.js
@@ -3,9 +3,13 @@ const Update = async function (res) {
   const { header, footer } = require('../components');
 
   const content = `<h3>Update on the Production</h3>
-<i>Update: Tuesday, March 4, 2025</i>
+<i>Update: Friday, March 21, 2025</i>
 <p>
 <ol>
+<li>The reviews of the first performance are beginning to arrive, and are wonderfully positive. Here is an example:
+<p>"This play seamlessly blended drama and humor, delivering an inspiring and thought-provoking experience as well as a message that leaves a lasting impact on the audience."</p>
+<p style="padding-left: 20px;">&mdash;Shelly H., from Oakland</p></li>
+<li>We now have ads running on Facebook and Instagram, and are getting good results. One result is very surprising, but tells us a lot. One ad performs much better than all the others. That ad features a large purple Linky Pinky hand, and also the sentence "Materialism is for morons." We will continue experimenting broadly with different kinds of ads, including those with challenging content.</li>
 <li>We had a tremendously successful opening night on March 1, with a sold out show, a great performance and an appreciative and enthusiastic audience.</li>
 <li><a href="/tickets">Tickets are available for a second performance on March 22nd</a>.</li>
 <li>The second performance is at <a href="https://www.phoenixtheatresf.org/" target="__blank">Phoenix Theater</a> in San Francisco's theater district.</li>
