2babcc9ca5f42729306a240a65a831a78c95de7b
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2025-04-15 13:23:58 -0700

    Update the "Update" page further

--- pages/Update.js
+++ pages/Update.js
@@ -6,13 +6,12 @@ const Update = async function (res) {
 <i>Update: Tuesday, April 15, 2025</i>
 <p>
 <ol>
+<li>The show has been renamed to "Planet A," and this means that all of the branding and marketing materials are being updated.</li>
+<li>The renaming of the show suggests that the show is now more explicitly about sustainability and this is indeed true.</li>
 <li>We have temporarily paused performing in order to concentrate on creating an extraordinary lighting design and lighting sequences, to add many enhancements to the multimedia aspects of the show, and also to practice with the UFO drone with flashing LEDs that flies around the theater and has become a character in its own right.</li>
+<li>We are making a full commitment to performing at <a href="https://www.phoenixtheatresf.org/" target="__blank">Phoenix Theater</a> in San Francisco's theater district.</li>
 <li>The reviews of the first performance are beginning to arrive, and are wonderfully positive.</li>
-<li>We now have ads running on Facebook and Instagram, and are getting good results. One result is very surprising, but tells us a lot. There is one ad that performs much better than all the others, and that ad features a large purple Linky Pinky hand, and also the sentence "Materialism is for morons." We will continue experimenting broadly with different kinds of ads, including those with challenging content.</li>
 <li>We had a tremendously successful opening night on March 1, with a sold out show, a great performance and an appreciative and enthusiastic audience.</li>
-<li><a href="/tickets">Tickets are available for a second performance on April 5th</a>.</li>
-<li>The second performance is at <a href="https://www.phoenixtheatresf.org/" target="__blank">Phoenix Theater</a> in San Francisco's theater district.</li>
-<li>We are now working with a lighting designer, Beth Cockrell, who will be designing the lighting for future performances.</li>
 <li>Many enhancements are being added to the multimedia assets for the show.</li>
 <li>We have added some choreography with flashlights to the open of the show.</li>
 <li>We have begun special training for the cast in order to become a "power team" of actors.</li>
