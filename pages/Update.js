0381f45952ac346f0eebb6e56bee93fd4f8edd29
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2025-04-15 13:19:34 -0700

    Update the "Update" page

--- pages/Update.js
+++ pages/Update.js
@@ -3,9 +3,10 @@ const Update = async function (res) {
   const { header, footer } = require('../components');
 
   const content = `<h3>Update on the Production</h3>
-<i>Update: Friday, March 21, 2025</i>
+<i>Update: Tuesday, April 15, 2025</i>
 <p>
 <ol>
+<li>We have temporarily paused performing in order to concentrate on creating an extraordinary lighting design and lighting sequences, to add many enhancements to the multimedia aspects of the show, and also to practice with the UFO drone with flashing LEDs that flies around the theater and has become a character in its own right.</li>
 <li>The reviews of the first performance are beginning to arrive, and are wonderfully positive.</li>
 <li>We now have ads running on Facebook and Instagram, and are getting good results. One result is very surprising, but tells us a lot. There is one ad that performs much better than all the others, and that ad features a large purple Linky Pinky hand, and also the sentence "Materialism is for morons." We will continue experimenting broadly with different kinds of ads, including those with challenging content.</li>
 <li>We had a tremendously successful opening night on March 1, with a sold out show, a great performance and an appreciative and enthusiastic audience.</li>
