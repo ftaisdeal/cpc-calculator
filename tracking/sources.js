c0d84255a9e657a4a18292cb5ca1a9177157c3c6
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2025-07-27 22:38:48 -0700

    Edit sources to correct Eventbrite

--- tracking/sources.js
+++ tracking/sources.js
@@ -1,6 +1,6 @@
 module.exports = [
     ['Backstage', 'backstage', '#88a', '10'],
     ['YouTube', 'yt', '#8a8', '100'],
-    ['Eventbrite', 'eb', '#a88', '0'],
+    ['Eventbrite', 'eventbrite', '#a88', '0'],
     ['Full Calendar', 'fc', '#8b8', '100']
   ];
