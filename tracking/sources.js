a8cdb50d608cab0d56bb0d19abe4f8bac3788b14
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2025-07-27 22:36:44 -0700

    Edit sources to add Eventbrite

--- tracking/sources.js
+++ tracking/sources.js
@@ -1,7 +1,6 @@
 module.exports = [
     ['Backstage', 'backstage', '#88a', '10'],
     ['YouTube', 'yt', '#8a8', '100'],
-    ['SF posters 9/1', 'sf-2025-09-01', '#a88', '100'],
-    ['EB posters 9/1', 'eb-2025-09-01', '#88b', '100'],
+    ['Eventbrite', 'eb', '#a88', '0'],
     ['Full Calendar', 'fc', '#8b8', '100']
   ];
